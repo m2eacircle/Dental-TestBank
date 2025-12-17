c082fe88750de7759864f1c8f7ac625e98f3cc77
m2eacircle <donguk.chai@gmail.com>
2025-12-17 10:10:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3151,15 +3151,6 @@ export default function ImprovedTestBankApp() {
     }));
   };
 
-  const shuffleArray = (array) => {
-    const shuffled = [...array];
-    for (let i = shuffled.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-    }
-    return shuffled;
-  };
-
   const startTest = (subject, subtopic = null, isRetake = false) => {
     try {
       // Store parent for back navigation
