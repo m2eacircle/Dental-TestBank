309f047bb76624d4792d3b53a97a56435d8fff9d
m2eacircle <donguk.chai@gmail.com>
2025-12-18 23:41:25 -0500

    Update lesson1-anatomy-and-physiology.js

--- src/questions/lesson1/lesson1-anatomy-and-physiology.js
+++ src/questions/lesson1/lesson1-anatomy-and-physiology.js
@@ -1,8 +1,44 @@
 // Lesson 1 - Anatomy and Physiology
-// Questions to be added
+// 13 topics covering human body systems
 
 export const questions = {
-  "Anatomy and Physiology": [
-    // Questions will be added here
+  "1. Introduction to the Human Body": [
+    // Questions to be added
+  ],
+  "2. Cells and Tissues": [
+    // Questions to be added
+  ],
+  "3. Integumentary System": [
+    // Questions to be added
+  ],
+  "4. Skeletal System": [
+    // Questions to be added
+  ],
+  "5. Muscular System": [
+    // Questions to be added
+  ],
+  "6. Nervous Tissue and Brain": [
+    // Questions to be added
+  ],
+  "7. Spinal Cord and Peripheral Nerves": [
+    // Questions to be added
+  ],
+  "8. Blood": [
+    // Questions to be added
+  ],
+  "9. Anatomy and Function of the Heart": [
+    // Questions to be added
+  ],
+  "10. Lymphatic System": [
+    // Questions to be added
+  ],
+  "11. Endocrine System": [
+    // Questions to be added
+  ],
+  "12. Digestive System": [
+    // Questions to be added
+  ],
+  "13. Respiratory System": [
+    // Questions to be added
   ]
 };
