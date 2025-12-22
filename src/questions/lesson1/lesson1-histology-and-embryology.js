16eaf071f48f330e84cb7298fdfd07b40f5abdae
m2eacircle <donguk.chai@gmail.com>
2025-12-21 21:33:38 -0500

    Update lesson1-histology-and-embryology.js

--- src/questions/lesson1/lesson1-histology-and-embryology.js
+++ src/questions/lesson1/lesson1-histology-and-embryology.js
@@ -1,8 +1,35 @@
 // Lesson 1 - Histology and Embryology
-// Questions to be added
+// Cellular biology, tissue structure, and prenatal development
 
 export const questions = {
-  "Histology and Embryology": [
-    // Questions will be added here
+  "1. The Cell": [
+    // Questions to be added
+  ],
+  "2. Basic Tissue – Basement Membrane": [
+    // Questions to be added
+  ],
+  "3. Basic Tissue – Muscle and Nerve": [
+    // Questions to be added
+  ],
+  "4. Overview of Prenatal Development": [
+    // Questions to be added
+  ],
+  "5. Embryonic Period": [
+    // Questions to be added
+  ],
+  "6. Development of the Face and Neck": [
+    // Questions to be added
+  ],
+  "7. Palate and Tongue": [
+    // Questions to be added
+  ],
+  "8. Tooth Development": [
+    // Questions to be added
+  ],
+  "9. Enamel Organ": [
+    // Questions to be added
+  ],
+  "10. Oral Mucosa": [
+    // Questions to be added
   ]
 };
