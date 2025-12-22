da6815ba855edf50eec7d711d0c017461f1993bc
m2eacircle <donguk.chai@gmail.com>
2025-12-21 22:08:44 -0500

    Update lesson1-introduction-to-statistics-and-research.js

--- src/questions/lesson1/lesson1-introduction-to-statistics-and-research.js
+++ src/questions/lesson1/lesson1-introduction-to-statistics-and-research.js
@@ -1,8 +1,38 @@
 // Lesson 1 - Introduction to Statistics and Research
-// Questions to be added
+// Research methodology and statistical analysis
 
 export const questions = {
-  "Introduction to Statistics and Research": [
-    // Questions will be added here
+  "1. Introduction to Research": [
+    // Questions to be added
+  ],
+  "2. Qualitative Research": [
+    // Questions to be added
+  ],
+  "3. Quantitative Research (Part 1)": [
+    // Questions to be added
+  ],
+  "4. Quantitative Research (Part 2)": [
+    // Questions to be added
+  ],
+  "5. Probability and Nonprobability Sampling Methods": [
+    // Questions to be added
+  ],
+  "6. Mixed Research": [
+    // Questions to be added
+  ],
+  "7. Essentials of Statistics (Part 1)": [
+    // Questions to be added
+  ],
+  "8. Essentials of Statistics (Part 2)": [
+    // Questions to be added
+  ],
+  "9. Developing Research Questions and Hypothesis": [
+    // Questions to be added
+  ],
+  "10. Evidence-Informed Practice and Literature Review": [
+    // Questions to be added
+  ],
+  "11. Clinical Trial": [
+    // Questions to be added
   ]
 };
