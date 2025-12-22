75c7162f77c72f3d4e67af880734d4762e9a325a
m2eacircle <donguk.chai@gmail.com>
2025-12-21 20:31:02 -0500

    Update lesson1-dental-hygiene-theory-i.js

--- src/questions/lesson1/lesson1-dental-hygiene-theory-i.js
+++ src/questions/lesson1/lesson1-dental-hygiene-theory-i.js
@@ -1,8 +1,59 @@
 // Lesson 1 - Dental Hygiene Theory I
-// Questions to be added
+// Comprehensive instrumentation and clinical techniques
 
 export const questions = {
-  "Dental Hygiene Theory I": [
-    // Questions will be added here
+  "1. Course Intro, Dental Equipment, and Positioning": [
+    // Questions to be added
+  ],
+  "2. Instrument Grasp": [
+    // Questions to be added
+  ],
+  "3. Instrument Design and Classification (Part 1)": [
+    // Questions to be added
+  ],
+  "4. Instrument Design and Classification (Part 2)": [
+    // Questions to be added
+  ],
+  "5. Principles of Ergonomics": [
+    // Questions to be added
+  ],
+  "6. Use of Dental Mouth Mirror": [
+    // Questions to be added
+  ],
+  "7. Finger Rest in the Anterior Sextants": [
+    // Questions to be added
+  ],
+  "8. Elements of an Effective Instrumentation Stroke": [
+    // Questions to be added
+  ],
+  "9. Periodontal Probes and Basic Probing Technique": [
+    // Questions to be added
+  ],
+  "10. Explorers": [
+    // Questions to be added
+  ],
+  "11. Technique Essentials - Supragingival Calculus Removal": [
+    // Questions to be added
+  ],
+  "12. Technique Essentials - Subgingival Calculus Removal": [
+    // Questions to be added
+  ],
+  "13. Universal Curets": [
+    // Questions to be added
+  ],
+  "14. Area-Specific Curets": [
+    // Questions to be added
+  ],
+  "15. Vital Signs": [
+    // Questions to be added
+  ],
+  "16. Fulcruming Techniques": [
+    // Questions to be added
+  ],
+  "17. Instruments Sharpening": [
+    // Questions to be added
+  ],
+  "18. Alternate Scalers": [
+    // Questions to be added
   ]
 };
