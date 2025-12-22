d6e273d655c6a2d090fab4f40ec2489856ef41f7
m2eacircle <donguk.chai@gmail.com>
2025-12-21 22:08:30 -0500

    Update lesson1-microbiology.js

--- src/questions/lesson1/lesson1-microbiology.js
+++ src/questions/lesson1/lesson1-microbiology.js
@@ -1,8 +1,38 @@
 // Lesson 1 - Microbiology
-// Questions to be added
+// Bacteriology, virology, immunology, and oral microbiology
 
 export const questions = {
-  "Microbiology": [
-    // Questions will be added here
+  "1. Basic Bacteriology": [
+    // Questions to be added
+  ],
+  "2. Basic Virology": [
+    // Questions to be added
+  ],
+  "3. Microbial Pathogenesis": [
+    // Questions to be added
+  ],
+  "4. Basic Immunology": [
+    // Questions to be added
+  ],
+  "5. Medical Bacteriology (Part 1)": [
+    // Questions to be added
+  ],
+  "6. Medical Bacteriology (Part 2)": [
+    // Questions to be added
+  ],
+  "7. Medical Bacteriology (Part 3)": [
+    // Questions to be added
+  ],
+  "8. Medical Virology & Mycology": [
+    // Questions to be added
+  ],
+  "9. Viral Hepatitis and AIDS": [
+    // Questions to be added
+  ],
+  "10. Oral Microbiology and Dental Caries": [
+    // Questions to be added
+  ],
+  "11. Microbiology of Periodontal Diseases and Dentoalveolar Infections": [
+    // Questions to be added
   ]
 };
