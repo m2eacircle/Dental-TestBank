87ce660a1aa4dc784e7d288297f5b063e294234b
m2eacircle <donguk.chai@gmail.com>
2026-01-04 14:30:57 -0500

    Add files via upload

--- src/questions/index.js
+++ src/questions/index.js
@@ -601,3 +601,14 @@ export const questionBankMetadata = {
     }
   }
 };
+
+// Export list of available lectures for the main screen
+export const availableLectures = [
+  "Lesson 1",
+  "Lesson 2",
+  "Lesson 3",
+  "Lesson 4",
+  "Board Exam",
+  "Board Exam 2"
+];
+
