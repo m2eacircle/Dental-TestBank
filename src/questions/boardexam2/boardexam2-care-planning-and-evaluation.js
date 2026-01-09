391ef43c39ee8842731be26d2dafec4b71a6eb94
m2eacircle <donguk.chai@gmail.com>
2026-01-08 23:16:32 -0500

    Add questions for Care Planning and Evaluation

--- src/questions/boardexam2/boardexam2-care-planning-and-evaluation.js
+++ src/questions/boardexam2/boardexam2-care-planning-and-evaluation.js
@@ -3,6 +3,11 @@
 
 export const questions = {
   "2 - Care Planning and Evaluation": [
-    // Questions to be added here
+    "1. Care Planning in Dental Hygiene",
+    "1. Care Planning in Dental Hygiene (Case Study)",
+    "2. Patient Evaluation",
+    "2. Patient Evaluation (Case Study)",
+    "3. Ongoing and Recall Care",
+    "3. Ongoing and Recall Care (Case Study)"
   ]
 };
