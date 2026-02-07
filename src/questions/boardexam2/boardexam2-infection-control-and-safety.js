70c2a0c56f3637deac95ba409820c58a1f91a1a4
m2eacircle <donguk.chai@gmail.com>
2026-02-06 23:28:24 -0500

    Refactor infection control questions structure

--- src/questions/boardexam2/boardexam2-infection-control-and-safety.js
+++ src/questions/boardexam2/boardexam2-infection-control-and-safety.js
@@ -2,7 +2,28 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Infection Control and Safety": [
+  "1. Infectious Diseases and Infection Control": [
+    // Questions to be added here
+  ],
+  "1. Infectious Diseases and Infection Control (Case Study)": [
+    // Questions to be added here
+  ],
+  "2. Protective Barriers for Safety": [
+    // Questions to be added here
+  ],
+  "2. Protective Barriers for Safety (Case Study)": [
+    // Questions to be added here
+  ],
+  "3. Infection Control in Clinical Care": [
+    // Questions to be added here
+  ],
+  "3. Infection Control in Clinical Care (Case Study)": [
+    // Questions to be added here
+  ],
+  "4. Dental Emergencies": [
+    // Questions to be added here
+  ],
+  "4. Dental Emergencies (Case Study)": [
     // Questions to be added here
   ]
 };
