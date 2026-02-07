1877ad52cd33f38006ccf29d17a92e7aa3e47bc9
m2eacircle <donguk.chai@gmail.com>
2026-02-07 13:39:34 -0500

    Update questions for Clinical Environment and Documentation

--- src/questions/boardexam2/boardexam2-clinical-environment-and-documentation.js
+++ src/questions/boardexam2/boardexam2-clinical-environment-and-documentation.js
@@ -2,7 +2,16 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Clinical Environment and Documentation": [
+  "1. Patient Reception and Ergonomics": [
+    // Questions to be added here
+  ]
+  "1. Patient Reception and Ergonomics (Case Study)": [
+    // Questions to be added here
+  ]
+  "2. Dental Hygiene Documentation": [
+    // Questions to be added here
+  ]
+  "2. Dental Hygiene Documentation (Case Study)": [
     // Questions to be added here
   ]
 };
