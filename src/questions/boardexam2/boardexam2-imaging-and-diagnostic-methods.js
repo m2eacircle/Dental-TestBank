42ce244eac4a14c0baf7b7b9d9eb903e7e235d1d
m2eacircle <donguk.chai@gmail.com>
2026-02-08 08:28:39 -0500

    Add questions for Imaging and Diagnostic Methods

--- src/questions/boardexam2/boardexam2-imaging-and-diagnostic-methods.js
+++ src/questions/boardexam2/boardexam2-imaging-and-diagnostic-methods.js
@@ -2,7 +2,22 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Imaging and Diagnostic Methods": [
+  "1. Dental X-Rays": [
+    // Questions to be added here
+  ],
+  "1. Dental X-Rays (Case Study)": [
+    // Questions to be added here
+  ],
+  "2. Dental Indices and Scoring": [
+    // Questions to be added here
+  ],
+  "2. Dental Indices and Scoring (Case Study)": [
+    // Questions to be added here
+  ],
+  "3. Dental Hygiene Diagnosis": [
+    // Questions to be added here
+  ],
+  "3. Dental Hygiene Diagnosis (Case Study)": [
     // Questions to be added here
   ]
 };
