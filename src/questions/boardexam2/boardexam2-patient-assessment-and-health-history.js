ea795fef0b7230d9a2ec32f6041a2d6ac84e58cf
m2eacircle <donguk.chai@gmail.com>
2026-02-07 20:50:50 -0500

    Refactor patient assessment questions structure

--- src/questions/boardexam2/boardexam2-patient-assessment-and-health-history.js
+++ src/questions/boardexam2/boardexam2-patient-assessment-and-health-history.js
@@ -2,7 +2,22 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Patient Assessment and Health History": [
+  "1. Patient Health Histories": [
+    // Questions to be added here
+  ],
+  "1. Patient Health Histories (Case Study)": [
+    // Questions to be added here
+  ],
+  "2. Measuring Vital Signs": [
+    // Questions to be added here
+  ],
+  "2. Measuring Vital Signs (Case Study)": [
+    // Questions to be added here
+  ],
+  "3. Oral and Facial Examination": [
+    // Questions to be added here
+  ],
+  "3. Oral and Facial Examination (Case Study)": [
     // Questions to be added here
   ]
 };
