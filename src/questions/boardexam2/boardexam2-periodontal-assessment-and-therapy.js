db79f5d7c459077c81aab55e4f2a6b022f597ac8
m2eacircle <donguk.chai@gmail.com>
2026-02-08 15:07:17 -0500

    Refactor questions structure for periodontal assessment

--- src/questions/boardexam2/boardexam2-periodontal-assessment-and-therapy.js
+++ src/questions/boardexam2/boardexam2-periodontal-assessment-and-therapy.js
@@ -2,7 +2,22 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Periodontal Assessment and Therapy": [
+  "1. Periodontal Assessment": [
+    // Questions to be added here
+  ],
+  "1. Periodontal Assessment (Case Study)": [
+    // Questions to be added here
+  ],
+  "2. Nonsurgical Periodontal Treatment": [
+    // Questions to be added here
+  ],
+  "2. Nonsurgical Periodontal Treatment (Case Study)": [
+    // Questions to be added here
+  ],
+  "3. Sutures and Periodontal Dressings": [
+    // Questions to be added here
+  ],
+  "3. Sutures and Periodontal Dressings (Case Study)": [
     // Questions to be added here
   ]
 };
