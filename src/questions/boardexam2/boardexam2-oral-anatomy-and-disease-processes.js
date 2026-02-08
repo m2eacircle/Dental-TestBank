34355d6ff52282858bbd013bbb5436d2f39f89d9
m2eacircle <donguk.chai@gmail.com>
2026-02-08 10:56:08 -0500

    Add new questions for Oral Anatomy and Disease Processes

--- src/questions/boardexam2/boardexam2-oral-anatomy-and-disease-processes.js
+++ src/questions/boardexam2/boardexam2-oral-anatomy-and-disease-processes.js
@@ -2,7 +2,28 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Oral Anatomy and Disease Processes": [
+  "1. Tooth Structure Examination": [
+    // Questions to be added here
+  ],
+  "1. Tooth Structure Examination (Case Study)": [
+    // Questions to be added here
+  ],
+  "2. Plaque, Calculus, and Stain": [
+    // Questions to be added here
+  ],
+  "2. Plaque, Calculus, and Stain (Case Study)": [
+    // Questions to be added here
+  ],
+  "3. Anatomy of the Periodontium": [
+    // Questions to be added here
+  ],
+  "3. Anatomy of the Periodontium (Case Study)": [
+    // Questions to be added here
+  ],
+  "4. Development of Periodontal Disease": [
+    // Questions to be added here
+  ],
+  "4. Development of Periodontal Disease (Case Study)": [
     // Questions to be added here
   ]
 };
