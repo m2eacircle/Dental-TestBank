9d9466f46a0c1a5bc66ecc59e9ba4b2a8f325e53
m2eacircle <donguk.chai@gmail.com>
2026-02-07 19:48:01 -0500

    Update questions structure in clinical environment file

--- src/questions/boardexam2/boardexam2-clinical-environment-and-documentation.js
+++ src/questions/boardexam2/boardexam2-clinical-environment-and-documentation.js
@@ -4,13 +4,13 @@
 export const questions = {
   "1. Patient Reception and Ergonomics": [
     // Questions to be added here
-  ]
+  ],
   "1. Patient Reception and Ergonomics (Case Study)": [
     // Questions to be added here
-  ]
+  ],
   "2. Dental Hygiene Documentation": [
     // Questions to be added here
-  ]
+  ],
   "2. Dental Hygiene Documentation (Case Study)": [
     // Questions to be added here
   ]
