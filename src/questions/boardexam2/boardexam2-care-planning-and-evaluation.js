0bab8a1723d73382f442adb4d3752bc465c58fff
m2eacircle <donguk.chai@gmail.com>
2026-02-08 20:09:22 -0500

    Refactor questions structure for Care Planning section
    Updated question structure for Care Planning and Evaluation section to include placeholders for future questions.

--- src/questions/boardexam2/boardexam2-care-planning-and-evaluation.js
+++ src/questions/boardexam2/boardexam2-care-planning-and-evaluation.js
@@ -2,12 +2,22 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Care Planning and Evaluation": [
-    "1. Care Planning in Dental Hygiene",
-    "1. Care Planning in Dental Hygiene (Case Study)",
-    "2. Patient Evaluation",
-    "2. Patient Evaluation (Case Study)",
-    "3. Ongoing and Recall Care",
-    "3. Ongoing and Recall Care (Case Study)"
+  "1. Care Planning in Dental Hygiene": [
+    // Questions to be added here
+  ],
+  "1. Care Planning in Dental Hygiene (Case Study)": [
+    // Questions to be added here
+  ],
+  "2. Patient Evaluation": [
+    // Questions to be added here
+  ],
+  "2. Patient Evaluation (Case Study)": [
+    // Questions to be added here
+  ],
+  "3. Ongoing and Recall Care": [
+    // Questions to be added here
+  ],
+  "3. Ongoing and Recall Care (Case Study)": [
+    // Questions to be added here
   ]
 };
