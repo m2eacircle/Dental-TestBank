8cd9e4ecea1e209ac813e7b481db200accaccf90
m2eacircle <donguk.chai@gmail.com>
2026-02-10 18:21:54 -0500

    Add files via upload

---
+++ src/questions/lesson2/lesson2-dental-radiography-interpretation.js
@@ -0,0 +1,26 @@
+// LESSON 2 - Dental Radiography Interpretation Questions
+
+// Topics:
+//   1. Radiation History & Radiation Physics (Part 1) (275 questions)
+//   2. Dental X-ray Equipment, Film Processing (Part 1) (255 questions)
+//   3. X-Ray Production and Equipment (Part 2) (53 questions) - NEW
+//   4. Film Mounting and Quality Control (Part 2) (37 questions) - NEW
+//   5. Film Processing Chemistry (Part 2) (47 questions) - NEW
+
+export const questions = {
+  "1. Fundamentals and Professional Practice": [
+    // Add questions here
+  ],
+  "2. Normal Anatomical Landmarks of the Maxilla and Tooth Structure": [
+    // Add questions here
+  ],
+  "3. Radiographic Anatomy of the Mandible and Periodontal Structures": [
+    // Add questions here
+  ],
+  "4. Normal Anatomical Landmarks on Panoramic Radiographs": [
+    // Add questions here
+  ],
+  "5. Film Mounting, Viewing Procedures, and Periodontal Disease Assessment": [
+    // Add questions here
+  ]
+};
