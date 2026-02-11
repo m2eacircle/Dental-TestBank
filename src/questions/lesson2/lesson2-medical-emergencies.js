2602c722633988f094db40e70dcdd0bc25fdb48d
m2eacircle <donguk.chai@gmail.com>
2026-02-10 21:06:54 -0500

    Add files via upload

---
+++ src/questions/lesson2/lesson2-medical-emergencies.js
@@ -0,0 +1,19 @@
+// LESSON 2 - Medical Emergencies Questions
+
+// Topics:
+//   1. Fundamentals and Professional Practice (148 questions)
+//   2. Normal Anatomical Landmarks of the Maxilla and Tooth Structure (152 questions)
+//   3. Radiographic Anatomy of the Mandible and Periodontal Structures (73 questions)
+//   4. Normal Anatomical Landmarks on Panoramic Radiographs (87 questions)
+//   5. Film Mounting, Viewing Procedures, and Periodontal Disease Assessment (201 questions)
+// 
+// TOTAL: 661 questions
+
+export const questions = {
+  "1. Introduction and prevention (Part 1)": [
+    // Add questions here
+  ],
+  "1. Introduction and prevention (Part 2)": [
+    // Add questions here
+  ]
+};
