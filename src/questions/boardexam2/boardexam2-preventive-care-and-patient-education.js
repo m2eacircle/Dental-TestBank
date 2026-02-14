bdae572388bf823b71ebb65c076cfb905ec1d319
m2eacircle <donguk.chai@gmail.com>
2026-02-14 08:24:40 -0500

    Update boardexam2-preventive-care-and-patient-education.js

--- src/questions/boardexam2/boardexam2-preventive-care-and-patient-education.js
+++ src/questions/boardexam2/boardexam2-preventive-care-and-patient-education.js
@@ -2,7 +2,52 @@
 // FIXED: Using prefixed name to avoid conflicts with Board Exam 1
 
 export const questions = {
-  "2 - Preventive Care and Patient Education": [
+  "1. Patient Education and Behavior Change": [
+    // Questions to be added here
+  ],
+  "1. Patient Education and Behavior Change (Case Study)": [
+    // Questions to be added here
+  ],
+  "2. Caries Prevention and Control": [
+    // Questions to be added here
+  ],
+  "2. Caries Prevention and Control (Case Study)": [
+    // Questions to be added here
+  ],
+  "3. Toothbrushing and Oral Hygiene": [
+    // Questions to be added here
+  ],
+  "3. Toothbrushing and Oral Hygiene (Case Study)": [
+    // Questions to be added here
+  ],
+  "4. Interdental Cleaning Methods": [
+    // Questions to be added here
+  ],
+  "4. Interdental Cleaning Methods (Case Study)": [
+    // Questions to be added here
+  ],
+  "5. Toothpaste and Mouthrinses": [
+    // Questions to be added here
+  ],
+  "5. Toothpaste and Mouthrinses (Case Study)": [
+    // Questions to be added here
+  ],
+  "6. Nutrition and Diet Assessment": [
+    // Questions to be added here
+  ],
+  "6. Nutrition and Diet Assessment (Case Study)": [
+    // Questions to be added here
+  ],
+  "7. Fluoride Use": [
+    // Questions to be added here
+  ],
+  "7. Fluoride Use (Case Study)": [
+    // Questions to be added here
+  ],
+  "8. Dental Sealants": [
+    // Questions to be added here
+  ],
+  "8. Dental Sealants (Case Study)": [
     // Questions to be added here
   ]
 };
