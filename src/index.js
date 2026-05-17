1fd71cef462663283ed2bbec255b669d2d273628
m2eacircle <donguk.chai@gmail.com>
2026-05-17 18:32:23 -0400

    Update service worker registration path

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register('./sw.js')
       .then(reg => console.log('SW registered, scope:', reg.scope))
       .catch(err => console.warn('SW registration failed:', err));
   });
