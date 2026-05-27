c0d52c0ba928d2ea5bcec1ab224e94f34e4a995c
m2eacircle <donguk.chai@gmail.com>
2026-05-27 16:41:04 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,13 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Register service worker for PWA install support
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('./sw.js')
-      .then(reg => console.log('SW registered, scope:', reg.scope))
-      .catch(err => console.warn('SW registration failed:', err));
-  });
-}
